5327d35db1af41f3eb1f0cf3e05ff84fdec9ba85
Alma <alma.soriaw@gmail.com>
2025-02-21 16:29:49 -0700

    updates

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ missing_df_after = pd.read_csv('missing_values_summary_after.csv')
 app = dash.Dash(__name__)
 server = app.server
 # Set up basic authentication
-app.server.config['BASIC_AUTH_USERNAME'] = 'my_add'
-app.server.config['BASIC_AUTH_PASSWORD'] = '12asasdas'
+app.server.config['BASIC_AUTH_USERNAME'] = 'group1'
+app.server.config['BASIC_AUTH_PASSWORD'] = 'norquestca'
 
 basic_auth = BasicAuth(app.server)
 
